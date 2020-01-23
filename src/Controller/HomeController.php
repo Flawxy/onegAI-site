809e160705e0e74fd031b213595447f7e83e5e14
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-23 18:21:18 +0100

    Update CSS & BDD management & views
    Update CSS;
    Update $repo->find commands @ DocController, HomeController & PostController;
    Update views : doc/index, home, post/index.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +12,12 @@ class HomeController extends Controller {
     /**
      * @Route("/", name="homepage")
      */
-    public function home() {
-        return $this->render(
-            'home.html.twig'
-        );
+    public function home(PostRepository $repo)
+    {
+        $lastPosts = $repo->findBy(array(), array('createdAt' => 'DESC'), 3);
+
+        return $this->render('home.html.twig', [
+            'lastPosts' => $lastPosts
+        ]);
     }
 }
