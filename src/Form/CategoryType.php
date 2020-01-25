d9e4458c66a43c2c703607dc84832c85b65c06b5
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-25 15:28:00 +0100

    Create categories creation functionality & Edit table of contents display
    :white_check_mark: Add CategoryController ;
    :white_check_mark: Add CategoryType
    :white_check_mark: Add category/new.html.twig ;
    :repeat: Edit doc/index.html.twig

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+
+    /**
+     * Permet d'avoir la configuration de base d'un champ du formulaire
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    public function getConfiguration($label, $placeholder)
+    {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                $this->getConfiguration("Nom", "Le nom de la nouvelle catégorie")
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                $this->getConfiguration("Description", "La description de la nouvelle catégorie")
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
